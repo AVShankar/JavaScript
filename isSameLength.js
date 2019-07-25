64091d3c986412e35793c6a061ba781a2df79f02
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-07-25 11:50:45 +0530

    Add files via upload

---
+++ isSameLength.js
@@ -0,0 +1,15 @@
+function isSameLength(word1, word2)
+{
+    var len1 = word1.length;
+    var len2 = word2.length;
+    if(len1==len2)
+    {
+        console.log("true");
+    }
+    else
+    {
+        console.log("false");
+    }
+}
+var str1="GUVI", str2="GEEK";
+isSameLength(str1, str2);
