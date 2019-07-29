77d100fe503751557fa6e9889cd8fc59011f608b
Shankar <52577232+AVShankar@users.noreply.github.com>
2019-07-29 14:39:25 +0530

    Create myClass.js

---
+++ myClass.js
@@ -0,0 +1,21 @@
+class guvi_student
+{
+    constructor(mark1, mark2)
+    {
+        this.mark1 = mark1;
+        this.mark2 = mark2;
+    }
+}
+const marks = new guvi_student (60, 70);
+console.log(marks);
+class guvi_students
+{
+    constructor(mark1)
+    {
+        this.mark1 = mark1;
+    }
+}
+const student1 = new guvi_students(90);
+console.log(student1);
+const student2 = new guvi_students(85);
+console.log(student2);
